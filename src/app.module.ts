b4f8521333d874e2a13a34967869db155277e52a
Bartlomiej Nowak <bartlomiej.krzysztof.nowak@gmail.com>
2023-11-13 12:15:44 +0100

    added conf for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Admin:admin@cluster0.9beonqd.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
